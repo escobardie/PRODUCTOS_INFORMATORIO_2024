74738136d0e6be1b556fe38d97f3f6e9940e6337
escobardie <diego_escobar@live.com>
2024-07-31 23:29:33 -0300

    CREACION DEL REPO
    PRACTICA PYTHON

---
+++ Sistema_Gestio_Producto.py
@@ -0,0 +1,253 @@
+# Desafío 1: Sistema de Gestión de Productos
+# Objetivo: Desarrollar un sistema para manejar productos en un inventario.
+# Requisitos:
+#     • Crear una clase base Producto con atributos como nombre, precio, cantidad en stock, etc.
+#     • Definir al menos 2 clases derivadas para diferentes categorías de productos (por ejemplo, ProductoElectronico, ProductoAlimenticio) con atributos y métodos específicos.
+#     • Implementar operaciones CRUD para gestionar productos del inventario.
+#     • Manejar errores con bloques try-except para validar entradas y gestionar excepciones.
+#     • Persistir los datos en archivo JSON.
+import json
+
+class Producto:
+    def __init__(self, codigo, nombre, precio,  stock, proveedor):
+        '''INICIALIZAMOS EN CONSTRUCTOR'''
+        self.__codigo = codigo
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__stock = stock
+        self.__proveedor = proveedor
+    
+    ### DEFINIMOS GETTER AND SETTER
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, value):
+        self.__codigo = value
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+
+    @property
+    def precio(self):
+        return self.__precio
+
+    @precio.setter
+    def precio(self, value):
+        self.__precio = value
+
+    @property
+    def stock(self):
+        return self.__stock
+
+    @stock.setter
+    def stock(self, value):
+        self.__stock = value
+
+    @property
+    def proveedor(self):
+        return self.__proveedor
+
+    @proveedor.setter
+    def proveedor(self, value):
+        self.__proveedor = value
+
+    def to_dict(self):
+        '''RETORNAMOS UN DICCIONARIO (CLAVE : VALOR)'''
+        return {
+            "codigo" : self.codigo,
+            "nombre" : self.nombre,
+            "precio" : self.precio,
+            "stock" : self.stock,
+            "proveedor" : self.proveedor
+        }
+
+    def __str__(self):
+        '''RETORNAMOS CODIGO Y NOMBRE'''
+        return f"Codigo: {self.codigo} - Nombre: {self.nombre}"
+
+
+class ProductoAlimento(Producto):
+    def __init__(self, codigo, nombre, precio,  stock, proveedor, fecha_vencimiento):
+        super().__init__(codigo, nombre, precio,  stock, proveedor)
+        self.__fecha_vencimiento = fecha_vencimiento
+
+    @property
+    def fecha_vencimiento(self):
+        return self.__fecha_vencimiento
+
+    @fecha_vencimiento.setter
+    def fecha_vencimiento(self, value):
+        self.__fecha_vencimiento = value
+
+    def to_dict(self):
+        '''RETORNAMOS UN DICCIONARIO (CLAVE : VALOR) CON UN ELEMENTO MAS'''
+        data = super().to_dict()
+        data["fecha_vencimiento"] = self.fecha_vencimiento
+        return data
+    
+    def __str__(self):
+        '''RETORNAMOS CODIGO, NOMBRE Y FECHA DE VENCIMIENTO'''
+        return f"{super().__str__()} - Fecha de vencimiento: {self.fecha_vencimiento}"
+
+
+class ProductoElectronico(Producto):
+    def __init__(self, codigo, nombre, precio,  stock, proveedor, meses_garantia):
+        super().__init__(codigo, nombre, precio,  stock, proveedor)
+        self.__meses_garantia = meses_garantia
+
+    @property
+    def meses_garantia(self):
+        return self.__meses_garantia
+
+    @meses_garantia.setter
+    def meses_garantia(self, value):
+        self.__meses_garantia = value
+
+    def to_dict(self):
+        '''RETORNAMOS UN DICCIONARIO (CLAVE : VALOR) CON UN ELEMENTO MAS'''
+        data = super().to_dict()
+        data["meses_garantia"] = self.meses_garantia
+        return data
+    
+    def __str__(self):
+        '''RETORNAMOS CODIGO, NOMBRE Y MESES DE GARANTIA'''
+        return f"{super().__str__()} - Garantia Validad por: {self.meses_garantia} meses"
+
+
+class CRUDProductos:
+    def __init__(self, archivo):
+        self.archivo = archivo
+    
+    def leer_datos(self):
+        try:
+            with open(self.archivo, 'r') as file:
+                datos = json.load(file)
+        except FileNotFoundError:
+            return {}
+        except Exception as error:
+            raise Exception(f'Error al leer datos del archivo: {error}')
+        else:
+            return datos
+
+    def guardar_datos(self, datos):
+        try:
+            with open(self.archivo, 'w') as file:
+                json.dump(datos, file, indent=4)
+
+        except IOError as error:
+            print(f'Error al intentar guardar los datos en {self.archivo}: {error}')
+        except Exception as error: #TODO  HAY QUE SER MAS CLAROS CON LOS TIPOS DE ERRORES
+            print(f'Error inesperado: {error}')
+
+    def crear_producto(self, producto, tipo_producto): ## SE ENVIARA EL TIPO DE PRODUCTO YA SELECCIONADO EN EL MENU MAIN
+        try:
+            datos = self.leer_datos()
+            codigo = producto.codigo
+
+            if tipo_producto == '1':
+                if 'productoElectronico' not in datos:
+                    datos['productoElectronico'] = {}
+                if str(codigo) not in datos['productoElectronico']:
+                    datos['productoElectronico'][codigo] = producto.to_dict()
+                    self.guardar_datos(datos)
+                    print(f"Producto ELECTRONICO {codigo} creado correctamente.") #TODO aca ver como mostrar en __str__
+                else:
+                    print(f"Ya existe producto con CODIGO: '{codigo}'.")
+
+            elif tipo_producto == '2':
+                if 'productoAlimenticio' not in datos:
+                    datos['productoAlimenticio'] = {}
+                if str(codigo) not in datos['productoAlimenticio']:
+                    datos['productoAlimenticio'][codigo] = producto.to_dict()
+                    self.guardar_datos(datos)
+                    print(f"Producto ALIMENTICIO {codigo} creado correctamente.") #TODO aca ver como mostrar en __str__
+                else:
+                    print(f"Ya existe producto con CODIGO: '{codigo}'.")
+            else:
+                    print("Erro entro por aca")                      
+
+        except Exception as error: #TODO HAY QUE SER MAS CLAROS CON LOS TIPOS DE ERRORES
+            print(f'Error inesperado: {error}')
+
+    def leer_producto(self, codigo, tipo_producto):
+        try:
+            datos = self.leer_datos()
+            if tipo_producto == '1':
+                if codigo in datos['productoElectronico']:
+                    print(f'Se encontró PRODUCTO con CODIGO {codigo}')
+                    for key, value in datos['productoElectronico'][codigo].items():
+                        print(f"    {key}: {value}")
+                else:
+                    print(f'No se encontró PRODUCTO con CODIGO {codigo}')
+            else:
+                if codigo in datos['productoAlimenticio']:
+                    print(f'Se encontró PRODUCTO con CODIGO {codigo}')
+                    for key, value in datos['productoAlimenticio'][codigo].items():
+                        print(f"    {key}: {value}")
+                else:
+                    print(f'No se encontró PRODUCTO con CODIGO {codigo}')
+
+        except Exception as error: #TODO HAY QUE SER MAS CLAROS CON LOS TIPOS DE ERRORES
+            print(f'Error inesperado: {error}')
+
+    def actualizar_producto(self, codigo, tipo_producto):
+        try:
+            datos = self.leer_datos()
+            
+            if tipo_producto == '1':
+                if codigo in datos['productoElectronico']:
+                    print(f'Se encontró PRODUCTO con CODIGO {codigo}')
+                    nuevo_ingreso = int(input('Ingrese cantidad del ingreso: '))
+                    datos['productoElectronico'][codigo]['stock'] += nuevo_ingreso
+                    #input('Presione enter para continuar...')
+                    self.guardar_datos(datos)
+                    
+                else:
+                    print(f'No se encontró PRODUCTO con CODIGO {codigo}')
+
+            else:
+                if codigo in datos['productoAlimenticio']:
+                    print(f'Se encontró PRODUCTO con CODIGO {codigo}')
+                    nuevo_ingreso = int(input('Ingrese cantidad del ingreso: '))
+                    datos['productoAlimenticio'][codigo]['stock'] += nuevo_ingreso
+                    #input('Presione enter para continuar...')
+                    self.guardar_datos(datos)
+
+                else:
+                    print(f'No se encontró PRODUCTO con CODIGO {codigo}')
+
+        except Exception as error: #TODO HAY QUE SER MAS CLAROS CON LOS TIPOS DE ERRORES
+            print(f'Error inesperado: {error}')
+
+    def eliminar_producto(self, codigo, tipo_producto):
+        try:
+            datos = self.leer_datos()
+            
+            if tipo_producto == '1':
+                if codigo in datos['productoElectronico']:
+                    print(f'Se encontró PRODUCTO con CODIGO {codigo}')
+                    del datos['productoElectronico'][codigo]
+                    input('Presione enter para ELIMINAR...')
+                    self.guardar_datos(datos)
+                    print(f'Producto CODIGO:{codigo} eliminado correctamente')
+                else:
+                    print(f'No se encontró PRODUCTO con CODIGO {codigo}')
+            else:
+                if codigo in datos['productoAlimenticio']:
+                    print(f'Se encontró PRODUCTO con CODIGO {codigo}')
+                    del datos['productoAlimenticio'][codigo]
+                    input('Presione enter para ELIMINAR...')
+                    self.guardar_datos(datos)
+                    print(f'Producto CODIGO:{codigo} eliminado correctamente')
+                else:
+                    print(f'No se encontró PRODUCTO con CODIGO {codigo}')
+
+        except Exception as error: #TODO HAY QUE SER MAS CLAROS CON LOS TIPOS DE ERRORES
+            print(f'Error inesperado: {error}')
---
+++ main.py
@@ -0,0 +1,161 @@
+from os import name, system
+import platform
+from time import sleep
+from Sistema_Gestio_Producto import ProductoAlimento, ProductoElectronico, CRUDProductos
+
+###################### LIMPIA PANTALLA ######################
+# Para Unix/Linux/MacOS/BSD
+if name == "posix":
+    limpiar = "clear"
+# Para DOS/Windows
+elif name == "ce" or name == "nt" or name == "dos":
+    limpiar = "cls"
+
+
+# def limpiar_pantalla():
+#     ''' Limpiar la pantalla según el sistema operativo'''
+#     if platform.system() == 'Windows':
+#         os.system('cls')
+#     else:
+#         os.system('clear') # Para Linux/Unix/MacOs
+
+################## ESPERA Y LIMPIEZA #######################
+
+def sleep_and_clear():
+    """ GENERAMOS UN TIEMPO DE ESPERA Y PUEGO EJECUTAMOS UNA LIMPIEZA DE PANTALLA"""
+    sleep(2)
+    system(limpiar)
+
+# time.sleep(3) tiempo de esperta antes de la siguiente ejecucion
+# os.system(limpiar) limpia la pantalla
+
+
+
+def menu_ppal():
+    menu = input('''
+        *******************************************
+        *       Sistema Analisis de Datos         *
+        *******************************************
+        *                                         *
+        *     1 - Add Producto                    *
+        *     2 - Buscar Producto X codigo        *
+        *     3 - Alctualizar Stock               *
+        *     4 - Eliminar Producto               *
+        *     5 - Mostrar Productos               *
+        *     6 - Salir                           *
+        *                                         *
+        *******************************************
+    Ingrese una opción: ''')
+    return menu
+
+
+def menu_tipo_producto():
+    opciones = ['1','2','3']
+    while True:
+        menu = input('''
+            *******************************************
+            *          MENU TIPO DE PRODUCTO          *
+            *******************************************
+            *                                         *
+            *     1 - Producto Electro                *
+            *     2 - Producto Alimento               *
+            *     3 - Salir                           *
+            *                                         *
+            *******************************************
+        Ingrese una opción: ''')
+        if str(menu) in opciones:
+            return menu
+        else:
+            print('Opción no válida. Por favor, seleccione una opción válida (1-3)')
+            
+
+def agregar_producto(control_productos, tipo_de_producto):
+    try:
+        #tipo_de_producto = menu_tipo_producto()
+        codigo = input('Ingrese CODIG: ')
+        nombre = input('Ingrese NOMBRE: ')
+        precio = float(input('Ingrese PRECIO: '))
+        stock = int(input('Ingrese Cantidad de STOCK: ') )
+        proveedor = input('Ingrese PROVEEDOR: ')
+        
+        if tipo_de_producto == '1':
+            meses_garantia = input('Ingrese Meses de Garantia: ')
+            producto = ProductoElectronico(codigo, nombre, precio,  stock, proveedor, meses_garantia)
+        elif tipo_de_producto == '2':
+            fecha_vencimiento = input('Ingrese Fecha de Vencimiento DD/MM/AAAA: ')
+            producto = ProductoAlimento(codigo, nombre, precio,  stock, proveedor, fecha_vencimiento)
+        else:
+            print("OCURRIO UN ERROR !ENTRO POR ACA!.")
+
+        control_productos.crear_producto(producto, tipo_de_producto)
+        input('ENTER para continuar...')
+        sleep_and_clear()
+
+    except Exception as error: #TODO HAY QUE SER MAS CLAROS CON LOS TIPOS DE ERRORES
+            print(f'Error inesperado: {error}')
+
+def buscar_producto_por_codigo(control_productos, tipo_de_producto):
+    codigo = input('Ingrese el CODIGO del PRODUCTO: ')
+    control_productos.leer_producto(codigo, tipo_de_producto)
+    input('Presione enter para continuar...')
+
+def actualizar_stock(control_productos, tipo_de_producto):
+    codigo = input('Ingrese el CODIGO del PRODUCTO: ')
+    control_productos.actualizar_producto(codigo, tipo_de_producto)
+    input('Presione enter para continuar...')
+
+def eliminar_producto_por_codigo(control_productos,tipo_de_producto):
+    codigo = input('Ingrese el CODIGO del PRODUCTO: ')
+    control_productos.eliminar_producto(codigo, tipo_de_producto)
+    input('Presione enter para continuar...')
+
+def mostrar_todos_los_productos(control_productos):
+    print('=============== Listado completo de los PRODUCTOS =============== ')
+    #producto = control_productos.leer_datos()#ORIGINAL
+    #for categoria, productos in producto.items(): #ORIGINAL
+    for categoria, productos in control_productos.leer_datos().items():
+        print(f"Categoría: {categoria}")
+        for producto_id, detalles in productos.items():
+            print(f"  Producto ID: {producto_id}")
+            for key, value in detalles.items():
+                print(f"    {key}: {value}")
+    print('================================================================== ')
+    input('Presione enter para continuar...')
+
+
+if __name__ == "__main__":
+    json_productos = 'productos_db.json'
+    control_productos = CRUDProductos(json_productos)
+
+    while True:
+        opcion = menu_ppal()
+        if opcion == '1': # AGREGAR PRODUCTO
+            tipo_producto = menu_tipo_producto()
+            agregar_producto(control_productos, tipo_producto)
+
+        elif opcion == "2": # BUSCAR POR CODIGO
+            tipo_producto = menu_tipo_producto()
+            buscar_producto_por_codigo(control_productos, tipo_producto)
+            
+        elif opcion == "3": # ACTUALIZAR
+            tipo_producto = menu_tipo_producto()
+            actualizar_stock(control_productos, tipo_producto)
+
+        elif opcion == "4": # ELIMINAR PRODUCTO
+            tipo_producto = menu_tipo_producto()
+            eliminar_producto_por_codigo(control_productos,tipo_producto)
+
+        elif opcion == "5": # MOSTRAR TODOS LOS PRODUCTOS
+            mostrar_todos_los_productos(control_productos)
+
+        elif opcion == "6": # SALIR
+            print('Saliendo del programa...')
+            break
+        
+        else:
+            system(limpiar)
+            print('Opción no válida. Por favor, seleccione una opción válida (1-7)')
+            sleep_and_clear()
+        
+
+
